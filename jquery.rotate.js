59669972739cc528c7fc46518885985c111c9139
冷子欲 <1058566903@qq.com>
2015-11-20 16:18:34 +0800

    add yasuo

---
+++ jquery.rotate.js
@@ -0,0 +1,35 @@
+;(function($){
+  $.fn.rotate = function(options){
+
+    //默认值
+  var defaults = {
+    speed:10,
+    width:"900px",
+    slide1:".slide1"
+  }
+  var options = $.extend(defaults, options);
+
+  this.each(function(){
+    var thisR = $(this),
+        thisC = thisR.children();
+    thisC.append('<ul class="slide2">'+ $(options.slide1).html() +'</ul>');
+    thisR.css({'overflow':'hidden','width':options.width});
+    thisC.css('width','4000px').children().css('float','left');
+    function Marquee(){
+      if(thisR.scrollLeft() >= $(options.slide1).width()){
+        thisR.scrollLeft(0);
+      }else{
+        thisR.scrollLeft(thisR.scrollLeft()+1);
+      }
+    }
+      var sliding=setInterval(Marquee,options.speed);
+      thisR.hover(function() {
+        //鼠标移动DIV上停止
+        clearInterval(sliding);
+      },function(){
+        //离开继续调用
+        sliding=setInterval(Marquee,options.speed);
+      });
+    });
+
+}})(jQuery);
